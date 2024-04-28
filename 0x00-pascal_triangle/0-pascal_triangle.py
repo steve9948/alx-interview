57f9cae3f643b5f0b09bfb91c7e267342faf6b20
steve9948 <mbuguagichimu@gmail.com>
2024-04-28 06:54:51 -0700

    Pascals error correction

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,14 +10,14 @@ def pascal_triangle(n):
         - n (int): The number of rows in the triangle.
 
     Returns:
-        - list of lists: A list containing lists representing each row of Pascal's Triangle.
+        - list of lists: A list with lists of each row of Pascal's Triangle.
     """
     triangle = []
-    
+
     for i in range(1, n + 1):
         row = [1] * i
         for j in range(2, i):
             row[j - 1] = triangle[i - 2][j - 2] + triangle[i - 2][j - 1]
         triangle.append(row)
-    
+
     return triangle
