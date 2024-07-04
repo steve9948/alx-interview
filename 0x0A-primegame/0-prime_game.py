4bb1a03638a19c0c3b09f233356b42e650822b7b
steve9948 <mbuguagichimu@gmail.com>
2024-07-04 11:43:05 -0700

    I mproved on time efficiency of the code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,56 +2,39 @@
 """0. Prime Game"""
 
 
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive"""
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+    for p in range(2, int(n**0.5) + 1):
+        if sieve[p]:
+            for multiple in range(p*p, n + 1, p):
+                sieve[multiple] = False
+    return [num for num in range(2, n + 1) if sieve[num]]
+
 def isWinner(x, nums):
-    if not nums or x < 1:
+    """
+    Determines the winner of Prime Game
+    x: the number of rounds
+    nums: list of the upper limit of ranges for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x < 1 or not nums:
         return None
 
-    def sieve(n):
-        """Return a list of primes up to n."""
-        is_prime = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if is_prime[p]]
-
-    max_n = max(nums)
-    primes = sieve(max_n)
-    prime_set = set(primes)
-
-    maria_wins = 0
-    ben_wins = 0
+    Maria = 0
+    Ben = 0
 
     for n in nums:
-        current_numbers = list(range(1, n + 1))
-        turn = 0  # 0 for Maria, 1 for Ben
+        prime_count = len(primes(n))
+        if prime_count % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
 
-        while True:
-            found_prime = False
-            for num in current_numbers:
-                if num in prime_set:
-                    found_prime = True
-                    prime = num
-                    break
-
-            if not found_prime:
-                if turn == 0:
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            multiples = [i for i in current_numbers if i % prime == 0]
-            for multiple in multiples:
-                current_numbers.remove(multiple)
-
-            turn = 1 - turn  # switch turns
-
-    if maria_wins > ben_wins:
+    if Maria > Ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif Ben > Maria:
         return "Ben"
-    else:
-        return None
+    return None
