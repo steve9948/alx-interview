933d6cf01a5e686fe7abb7d20a63fd54a68068a2
steve9948 <mbuguagichimu@gmail.com>
2024-07-04 11:52:16 -0700

    More boring game as checker needs

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,7 +3,15 @@
 
 
 def primes(n):
-    """Return list of prime numbers between 1 and n inclusive"""
+    """
+    Return a list of prime numbers between 1 and n inclusive.
+
+    Args:
+        n (int): The upper limit of the range to find primes.
+
+    Returns:
+        list: A list of prime numbers between 1 and n inclusive.
+    """
     sieve = [True] * (n + 1)
     sieve[0] = sieve[1] = False
     for p in range(2, int(n**0.5) + 1):
@@ -15,27 +23,32 @@ def primes(n):
 
 def isWinner(x, nums):
     """
-    Determines the winner of Prime Game
-    x: the number of rounds
-    nums: list of the upper limit of ranges for each round
-    Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+    Determines the overall winner of the Prime Game after x rounds.
+
+    Args:
+        x (int): The number of rounds to be played.
+        nums (list): A list of integers where each integer represents the upper
+                     limit of the range of numbers for that round.
+
+    Returns:
+        str: The name of the player with the most wins ('Maria' or 'Ben').
+             Returns None if there is a tie or if the input is invalid.
     """
     if x < 1 or not nums:
         return None
 
-    Maria = 0
-    Ben = 0
+    maria_wins = 0
+    ben_wins = 0
 
     for n in nums:
         prime_count = len(primes(n))
         if prime_count % 2 == 0:
-            Ben += 1
+            ben_wins += 1
         else:
-            Maria += 1
+            maria_wins += 1
 
-    if Maria > Ben:
+    if maria_wins > ben_wins:
         return "Maria"
-    elif Ben > Maria:
+    elif ben_wins > maria_wins:
         return "Ben"
     return None
